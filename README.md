42933e3a097e0c00ec87a2f7054426f83a0cb02a
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-08-13 01:48:07 +1000

    done for now

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# create_colour
+# final-project
+
+frame_id: 822300
+num_hands: 1
+num_fingers: 5
+
+hand_type: right
+pinch_strength: 0
+grab_strength: 1
+confidence: 0.307861
+
+finger_thumb_dip: -8.58221, 154.551, -0.789657
+finger_type: 0
+(Thumb)
+finger_dip: -8.58221,154.551,-0.789657
+finger_pip: 23.6399,143.609,1.33483
+finger_mcp: 71.5556,130.926,-4.58046
+
+finger_type: 1
+(Index)
+finger_dip: -13.6007,234.853,-16.4643
+finger_pip: 5.42027,237.394,-1.76897
+finger_mcp: 30.6206,202.804,2.00276
+
+finger_type: 2
+(Middle)
+finger_dip: 19.7196,159.358,-40.8353
+finger_pip: 9.84553,185.832,-37.6301
+finger_mcp: 43.638,210.799,-14.0084
+
+finger_type: 3
+(Ring)
+finger_dip: 32.2142,163.341,-51.2191
+finger_pip: 25.2219,190.142,-50.7245
+finger_mcp: 58.6849,212.271,-31.0574
+
+finger_type: 4
+(Pinky)
+finger_dip: 43.129,175.072,-58.4352
+finger_pip: 40.6337,194.404,-60.0505
+finger_mcp: 70.4772,209.62,-48.732
--- js/main.js
+++ js/main.js
@@ -1,39 +1,16 @@
 
-
 $( document ).ready(function() {
 
+
   Leap.loop(function(frame){
+    if (frame.hands.length === 1) {
+      const index = frame.hands[0].fingers[1].tipPosition.map(function(p){
+        return parseInt(Math.abs(p) / 600 * 255);
+      }).join(',');
 
-    var hand = frame.hands[0];
-
-    var red = hand.fingers[1].tipPosition[0]
-    var green = hand.fingers[1].tipPosition[1]
-    var blue = hand.fingers[1].tipPosition[2]
-
-    // var red = 200
-    // var green = 200
-    // var blue = 200
-
-
-    var r = red.toString();
-    var g = green.toString();
-    var b = blue.toString();
-
-
-    for (var i = 0; i < frame.hands.length; i++) {
-
-      // $('#output').css("background", "rgb("r + "," + g + "," + b")")
-      $('body').css("background", "rgb(" + r + "," + g + "," + b + ")")
-
+      const rgb = `rgb(${index})`;
+      window.document.body.style.backgroundColor = rgb;
 
     }
-
-    // output.innerHTML = framestring
-
-    //attach to dom here, yo.
-    // body.css()
-
-
-
   })
 })
