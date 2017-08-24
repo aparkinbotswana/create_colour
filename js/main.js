cc82c85514b35551c9f5e349b842b3d051fd53ce
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-08-24 12:33:46 +1000

    ????

--- js/main.js
+++ js/main.js
@@ -6,16 +6,11 @@ $( document ).ready(function() {
     if (frame.hands.length === 1) {
       const index = frame.hands[0].fingers[1].tipPosition.map(function(p){
         return parseInt(Math.abs(p) / 600 * 255);
-      }).join(',');
+      }).join(','); // takes the xyz coordinates of the tip of the index finger and turns it into an array.
 
       const rgb = `rgb(${index})`;
-      window.document.body.style.backgroundColor = rgb;
-      $('body').html(rgb).css('color', rgb).css('filter', 'invert(1)')
-
-
+      window.document.body.style.backgroundColor = rgb; // passes the array value in as RGB for the background colour.
+      $('body').html(rgb).css('color', rgb,).css('filter', 'invert(1)') //makes the text colour inverse to background colour.
     }
   })
 })
-
-
-// css property to invert colour = filter: invert(1)
